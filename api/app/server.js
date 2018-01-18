3fe31733defa684aa7f0cc6dd9b052b86404fb03
ivatsu <ivan.tsud@gmail.com>
2018-01-18 19:49:54 +0200

    added .env file and dotenv dependency

--- api/app/server.js
+++ api/app/server.js
@@ -1,9 +1,9 @@
 import app from './app';
-import { PORT } from './config';
+import { PORT, MONGO_URI } from './config';
 
 const server = app.listen(PORT, (err) => {
     if (err) console.log(err);
-
+    console.log(MONGO_URI);
     console.log(`Server is running on port: ${PORT}`);
     console.log(`Open your browser at localhost:${PORT}`);
 });
